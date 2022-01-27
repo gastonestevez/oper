10a5ab18b87d598ced0d30e240752f2a294155e0
tomasdim <79215198+tomasdim@users.noreply.github.com>
2022-01-27 08:03:31 -0300

    created ui

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Register from "./pages/Register";
+import Tracker from "./pages/Tracker";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Register />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
